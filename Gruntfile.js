ece09784dbd91c204bc26ae0ea23bfbbfdcface2
Laura Luiz <laura.luiz@commercetools.de>
2015-11-15 22:04:35 +0100

    Add basic example

---
+++ Gruntfile.js
@@ -0,0 +1,115 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+
+    // Configuration of 'grunt-contrib-clean' task, to remove all output folder
+    clean: {
+      build: ['output/'],
+      dist: ['*.jar']
+    },
+
+    // Configuration of 'grunt-contrib-copy' task, to move files into the output folder
+    copy: {
+      dist: {
+        files: [
+          { expand: true, cwd: 'input/', dest: 'output/', src: 'assets/**/*' },
+          { expand: true, cwd: 'input/templates/partials/', dest: 'output/templates/', src: '**/*.hbs' },
+          { expand: true, cwd: 'locales/', dest: 'output/locales', src: '**/*.yaml' }
+        ]
+      },
+      pages: {
+        files: [
+          { expand: true, cwd: 'input/', dest: 'output/', src: 'templates/*.hbs' }
+        ]
+      }
+    },
+
+    // Configuration of 'grunt-compile-handlebars' task, to compile Handlebars files and JSON into HTML
+    'compile-handlebars': {
+      dist: {
+        files: [{
+            expand: true,
+            cwd: 'output/templates',
+            src: '*.hbs',
+            dest: 'output/',
+            ext: '.html'
+        }],
+        templateData: '*.json', // compile-handlebars uses the template folder no matter what
+        partials: 'input/templates/partials/**/*.hbs',
+        helpers: 'input/templates/helpers/**/*.js'
+      }
+    },
+
+    // Configuration of 'grunt-maven-tasks' task, to generate the webjar and then install locally or deploy to bintray
+    maven: {
+      options: {
+        type: "jar",
+        groupId: 'com.lauraluiz',
+        artifactId: "<%= pkg.name %>",
+        version: "<%= pkg.version %>",
+        destFolder: "/META-INF/resources/webjars",
+        gitpush: true,
+        mode: "patch"
+      },
+      install : {
+        options : {
+          goal: "install"
+        },
+        files: [
+          { expand: true, cwd: 'output/assets/', src: "**/*", filter: "isFile" },
+          { expand: true, cwd: 'output/', src: "templates/**/*", filter: "isFile" },
+          { expand: true, cwd: 'output/', src: "locales/**/*", filter: "isFile" }
+        ]
+      }
+    },
+
+    // Configuration of the 'i18next' task, to support internationalization in Handlebars
+    i18n: {
+      options: {
+        preload: ['de', 'en'],
+        lng: 'en',
+        fallbackLng: 'en',
+        ns: {
+          namespaces: ['cart'],
+          defaultNs: 'cart'
+        }
+      }
+    },
+
+    // Configuration of 'json-refs' task, to resolve JSON references
+    'json-refs': {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'input/templates',
+          src: '*.json',
+          dest: 'output/templates',
+          ext: '.json'
+        }]
+      }
+    }
+  });
+
+  grunt.loadTasks('tasks');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-compile-handlebars');
+  grunt.loadNpmTasks('grunt-maven-tasks');
+
+  grunt.registerTask('default', 'build');
+  grunt.registerTask('build', ['clean', 'pre-handlebars', 'handlebars', 'copy:dist']);
+  grunt.registerTask('release', ['build', 'maven:install', 'clean:dist']);
+
+  grunt.registerTask('pre-handlebars', 'Tasks to be run before Handlebars', function() {
+    grunt.task.run('json-refs');
+    grunt.task.run('i18n');
+    grunt.task.run('copy:pages');
+  });
+
+  grunt.registerTask('handlebars', 'Compiles Handlebars templates using JSON data', function() {
+    grunt.task.requires('pre-handlebars');
+    grunt.task.run('compile-handlebars');
+  });
+
+};
--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # handlebars-template-demo
+
+###Installation
+
+- Install [Node.js](https://nodejs.org/) and [NPM](https://www.npmjs.com/)
+- Install [Grunt](http://gruntjs.com/getting-started)
+- Run `npm install` in the project root to install the project dependencies.
+
+###Usage
+
+#####Useful commands
+
+`grunt` to execute the tasks `build` and `release`
+
+`grunt build` to build the generated site
+
+`grunt release` to install to local maven repository (~/.m2/repository/io/sphere/sphere-sunrise-design)
+
+Notice you can always add `--verbose` and/or `--debug` to any command in order to obtain more information.
+
+#####Generated site
+
+Once the project is built, the generated site is located in the `output/` folder.
+
+#####Executed tasks
+
+`grunt clean`
+  - Removes the `output/` folder with the generated site
+  - Removes the generated JAR file
+
+`grunt json-refs`
+  - Resolves all JSON References found in files in `input/templates/` and creates a fully resolved equivalent in `output/templates/`
+
+`grunt compile-handlebars`
+  - Generates HTML files from the Handlebars templates and JSON data defined in `input/templates/` and the partial templates defined in `input/templates/partials/` into `output/`
+
+`grunt copy`
+  - Copy any asset in `input/assets/` to `output/assets/`
+  - Copy any Handlebars template in `input/templates/` to `output/templates/` (it flattens any directory)
+  - Copy any internationalization file from `locales/` to `output/locales/`  
